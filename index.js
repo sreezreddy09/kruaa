2279f5c845754d20b912aff538bda54fa8acb2cf
Srikanth Reddy <Srikanth@Srikanths-MacBook-Pro.local>
2017-11-05 23:01:48 -0800

    Updated the port no.

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get('/', function(request, response){
 app.use(express.static(__dirname + '/dist'));
 
 //App opens localhost on 3000...
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Application listening on port 3000....");
 });
