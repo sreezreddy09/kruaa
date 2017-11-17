b954ffd59013854793a82f5a19d3a48fcfe64f12
Srikanth Reddy <sreezreddy09@users.noreply.github.com>
2017-11-16 19:56:04 -0800

    main.js update

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from "react-redux";
-import configureStore from "./stores/configureStore.js";
+import configureStore from "./stores/ConfigureStore.js";
 import LoginPageContainer  from "./components/login/loginPageContainer";
 import MainDashboard from "./components/dashboard/MainDashboard.container";
 
