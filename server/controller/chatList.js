b002eb053087a8380ccdce4689c64be7765ee187
Srikanth Reddy <sreezreddy09@users.noreply.github.com>
2018-08-03 15:16:40 -0700

    Updated server-side controllers

--- server/controller/chatList.js
+++ server/controller/chatList.js
@@ -1,15 +1,46 @@
 const PostGresDriver = require("../services/postgresDriver");
-let FETCH_CHATS_CQL = "SELECT * FROM USERINFO;";
-
+let FETCH_CHATS_CQL = "SELECT conversation_id, users_bond, last_message, updated_time FROM CHAT_LIST WHERE LOWER(USERS_BOND) LIKE LOWER($1);";
+let FETCH_CHAT_MEMBER_INFO_CQL = "SELECT USER_UID, FIRST_NAME, LAST_NAME FROM USER_INFO WHERE USER_NAME = $1;"
+let FETCH_RECENT_CHAT_CQL = "SELECT MESSAGE, CREATEDAT_TIME FROM MESSAGES WHERE CONVERSATION_ID = $1 ORDER BY CREATEDAT_TIME DESC LIMIT 1;";
 class chatList {
 	constructor(options){
 		this.PostGresDriver = PostGresDriver
 	}
 
-	fetchChatList (){
-		return this.PostGresDriver.executeQuery({cql : FETCH_CHATS_CQL})
+	fetchChatList (query){
+		let regex = '%' + query + '%';
+		return this.PostGresDriver.executeQuery({cql : FETCH_CHATS_CQL, keys:[regex]})
             .then((data) => {
-                return data;
+				let chat_users = data.map((chat_member) => {
+					let users = chat_member.users_bond.split("::");
+					let user_name;
+					if(users[0] === query){
+						user_name = users[1];
+					}else{
+						user_name = users[0]
+					}
+
+					delete chat_member.users_bond;
+
+					return this.PostGresDriver.executeQuery({cql : FETCH_CHAT_MEMBER_INFO_CQL, keys: [user_name]})
+					.then((info) => {
+						chat_member["user_uid"] = info[0]["user_uid"];
+						chat_member["name"] = `${info[0]["first_name"]} ${info[0]["last_name"]}`;
+						return this.PostGresDriver.executeQuery({cql : FETCH_RECENT_CHAT_CQL, keys: [chat_member.conversation_id]})
+						.then((message) => {
+							if(message.length){
+								chat_member["last_message"] = message[0]["message"];
+								chat_member["updated_time"] = message[0]["createdat_time"]
+							}
+							return chat_member;
+						})
+					}).catch((err) => {
+						console.log("*********************", err);
+						return err;
+					});
+				});
+
+				return Promise.all(chat_users);
             });
 	}
 }
--- server/controller/userAuthentication.js
+++ server/controller/userAuthentication.js
@@ -1,7 +1,7 @@
 const PostGresDriver = require("../services/postgresDriver");
-let USER_LOGIN_CQL = "SELECT * FROM USERINFO WHERE USER_NAME = $1 AND PASSWORD = $2;"; 
-let USER_FETCH_CQL = "SELECT * FROM USERINFO WHERE id = $1;";
-let USER_SIGNON_CQL = "INSERT INTO USERINFO (NAME, USER_NAME, PASSWORD) VALUES ($1, $2, $3);";
+let USER_LOGIN_CQL = "SELECT * FROM USER_INFO WHERE USER_NAME = $1 AND PASSWORD = $2;"; 
+let USER_FETCH_CQL = "SELECT * FROM USER_INFO WHERE user_uid = $1;";
+let USER_SIGNON_CQL = "INSERT INTO USER_INFO (NAME, USER_NAME, PASSWORD) VALUES ($1, $2, $3);";
 
 class UserAuth {
     constructor (options){
@@ -12,14 +12,26 @@ class UserAuth {
     userSignIn(param){
         return this.PostGresDriver.executeQuery({cql : USER_LOGIN_CQL, keys : [param.user_name, param.password]})
             .then((data) => {
-                return data;
+                let user_record = {
+                    user_uid : data[0]["user_uid"],
+                    user_name : data[0]["user_name"],
+                    email_id : data[0]["email_id"],
+                    name : `${data[0]["first_name"]} ${data[0]["last_name"]}`
+                }
+                return user_record;
             });
     }
 
     fetchUser (param){
         return this.PostGresDriver.executeQuery({cql : USER_FETCH_CQL, keys : [param]})
             .then((data) => {
-                return data;
+                let user_record = {
+                    user_uid : data[0]["user_uid"],
+                    user_name : data[0]["user_name"],
+                    email_id : data[0]["email_id"],
+                    name : `${data[0]["first_name"]} ${data[0]["last_name"]}`
+                }
+                return [user_record];
             });
     }
 
