29f620fea0f5cd3ea593382b862da6aa890a027c
Srikanth Reddy <sreezreddy09@users.noreply.github.com>
2018-08-25 05:13:32 -0700

    Updated websocket to reconnect to chat-room on disconnect

--- server/services/server-socket.js
+++ server/services/server-socket.js
@@ -4,19 +4,38 @@ const logger = require("../services/logger");
 const chatHistoryController = require("../controller/chatHistory");
 
 module.exports.listen = (app) => {
-	let io = socketio.listen(app);
 
+	let io = socketio.listen(app);
 	let users = io.of("/users");
+
 	users.on("connection", (socket) => {
 
 		socket.on("join", (data) => {
 			logger.info("Creating chat room for user", data);
 			socket.join(data);
 		})
+
 		socket.on("new message", (data) => {
 			socket.to(data.user).emit('new message', data.message);
 			chatHistoryController.appendMessageToDB(data.message);
 		})
+
+		socket.on("chat profile", (data) => {
+			socket.to(data.user).emit("chat profile", data.info);
+		});
+
+		socket.on("disconnect", (reason) => {
+			logger.log("info", "socket disconnected", reason);
+		});
+
+		socket.on("error", (reason) => {
+			logger.log("info", "socket error", reason);
+		});
+
+		socket.on("disconnecting", (reason) => {
+			logger.log("info", "socket disconnecting", reason);
+		});
+
 	});
 
 	return io;
--- src/models/client-socket.js
+++ src/models/client-socket.js
@@ -5,8 +5,10 @@ import {store} from "../main";
 import { updateContactProfiles } from './contactsHelper';
 
 
-const socket = io('/users');
 
+const socket = io('/users', {
+	transports: ['websocket']
+});
 export function sendMessageToSocket(user, data){
 	socket.emit("new message", {
 		user : user,
@@ -19,9 +21,38 @@ export function createSocketUser(user){
 	socket.emit("join", user);
 }
 
+export function sendChatInfoToSocket (user_uid, chat_info){
+	let chatProfiles = [chat_info].concat(store.getState().user_profiles.users);
+	store.dispatch(update_user_profiles(chatProfiles));
+	let profile = {...chat_info, name : store.getState().user_info.user.name}
+	socket.emit("chat profile", {
+		user : user_uid,
+		info : profile
+	})
+}
+
 socket.on("new message", function(data){
-	let contactProfiles = updateContactProfiles(data.conversation_id, store.getState().user_profiles.users, data);
-	store.dispatch(update_user_profiles(contactProfiles));
+	let chatProfiles = updateContactProfiles(data.conversation_id, store.getState().user_profiles.users, data);
+	store.dispatch(update_user_profiles(chatProfiles));
 	store.dispatch(append_message_to_chat(data.conversation_id, data))
 });
 
+socket.on("chat profile", function(data){
+	let chatProfiles = [data].concat(store.getState().user_profiles.users);
+	store.dispatch(update_user_profiles(chatProfiles));
+})
+
+socket.on("disconnect", function(reason){
+	console.log("socket disconnected", reason);
+	socket.connect();
+	socket.emit("join", store.getState().user_info.user.user_uid);
+});
+
+socket.on('reconnecting', (attemptNumber) => {
+	console.log("socket reconnecting", attemptNumber);
+  });
+
+socket.on('reconnect_failed', () => {
+	console.log("socket reconnecting");
+
+});
