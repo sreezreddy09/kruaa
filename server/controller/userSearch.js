007b99d8a5a5722e9d141f6be8ef4d1e72b73731
Srikanth Reddy <sreezreddy09@users.noreply.github.com>
2018-08-25 05:14:56 -0700

    Updated user search controller

--- server/controller/userSearch.js
+++ server/controller/userSearch.js
@@ -1,5 +1,8 @@
 const PostGresDriver = require("../services/postgresDriver");
-let SEARCH_USERS_CQL = "SELECT NAME, USER_NAME FROM USERINFO WHERE LOWER(NAME) LIKE LOWER($1);";
+const logger = require("../services/logger");
+let SEARCH_USERS_CQL = "SELECT USER_UID, FIRST_NAME, LAST_NAME, USER_NAME FROM USER_INFO WHERE LOWER(FIRST_NAME) LIKE LOWER($1) OR LOWER(LAST_NAME) LIKE LOWER($1);";
+
+let SEARCH_REQUESTS_CQL = "SELECT * FROM FRIEND_REQUESTS WHERE USERS_BOND = $1;";
 
 
 class userSearch {
@@ -7,11 +10,33 @@ class userSearch {
 		this.PostGresDriver = PostGresDriver
 	}
 
-	searchUser(query){
+	searchUser(query, user_name){
         var regex = query + '%';
         return this.PostGresDriver.executeQuery({cql: SEARCH_USERS_CQL, keys: [regex]})
             .then((data) => {
-                return data;
+                let searchResults = data.filter((d) => d.user_name !== user_name);
+                let users = searchResults.map((member) => {
+                    let user = {
+                        "user_uid" : member.user_uid,
+                        "name" : `${member.first_name} ${member.last_name}`
+                    };
+                    
+                    let users_bond = [user_name, member.user_name].sort().join("::");  
+                    return this.PostGresDriver.executeQuery({cql : SEARCH_REQUESTS_CQL, keys : [users_bond]})
+                        .then((info) => {
+                            if(info.length){
+                                user["status"] = info[0]["request_status"];
+                            }else {
+                                user["status"] = "none";
+                            }
+                            return user;
+                        }).catch((err) => {
+                            logger.log("error","error in searching user", err);
+                            return err;
+                        })
+               })
+
+               return Promise.all(users);
             });
     }
 }
--- server/routes/userSearchRouter.js
+++ server/routes/userSearchRouter.js
@@ -4,16 +4,16 @@ const userSearchController = require("../controller/userSearch");
 const contactsRouter = express.Router();
 
 contactsRouter.use("/search", (req, res) => {
-	if(!req.query.q){
+    if(!req.query.q){
         return res.send([]);
-	}
+    }
 
-	userSearchController.searchUser(req.query.q).then((data) => {
+    userSearchController.searchUser(req.query.q, req.query.user).then((data) => {
         res.send(data);
     }).catch((err) => {
         logger.info("ERROR in searching user from DB", err);
         res.status(400).send(err);
-	});
+    });
 })
 
 module.exports = contactsRouter;
